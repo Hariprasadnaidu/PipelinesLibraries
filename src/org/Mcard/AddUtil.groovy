524098a85f8832b15c4a741b7c15cc1085e856c8
hariprasadnaidu.k@gmail.com <hariprasadnaidu.k@gmail.com>
2018-07-13 14:37:57 -0400

    addutilmodified

--- src/org/Mcard/AddUtil.groovy
+++ src/org/Mcard/AddUtil.groovy
@@ -9,8 +9,8 @@ public AddUtil(steps)  {this.steps=steps}
 public void sum(script, int a, int b)
 {
  def c= a + b
-def d = 10
+ d = 10
 steps.echo "This ia d value : ${script.env.d}".toInteger()
-steps.echo "${script.env.c}".toInteger()
+steps.echo "${script.env.c}"
 }
 }
